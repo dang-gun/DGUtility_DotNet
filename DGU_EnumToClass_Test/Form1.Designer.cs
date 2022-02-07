8d8c51db3e0831831fbdb969e7b14c7c03b8d994
최영동(y&j) <c1dong8@nate.com>
2022-02-07 17:04:50 +0900

    DGU_EnumToClass 프로젝트 생성

---
+++ DGU_EnumToClass_Test/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DGU_EnumToClass_Test
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnTest1 = new System.Windows.Forms.Button();
+			this.btnTest2 = new System.Windows.Forms.Button();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.SuspendLayout();
+			// 
+			// btnTest1
+			// 
+			this.btnTest1.Location = new System.Drawing.Point(12, 12);
+			this.btnTest1.Name = "btnTest1";
+			this.btnTest1.Size = new System.Drawing.Size(75, 23);
+			this.btnTest1.TabIndex = 0;
+			this.btnTest1.Text = "Test1";
+			this.btnTest1.UseVisualStyleBackColor = true;
+			this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+			// 
+			// btnTest2
+			// 
+			this.btnTest2.Location = new System.Drawing.Point(93, 12);
+			this.btnTest2.Name = "btnTest2";
+			this.btnTest2.Size = new System.Drawing.Size(75, 23);
+			this.btnTest2.TabIndex = 1;
+			this.btnTest2.Text = "Test2";
+			this.btnTest2.UseVisualStyleBackColor = true;
+			this.btnTest2.Click += new System.EventHandler(this.btnTest2_Click);
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(12, 41);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(306, 285);
+			this.listView1.TabIndex = 2;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(327, 340);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.btnTest2);
+			this.Controls.Add(this.btnTest1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnTest1;
+		private System.Windows.Forms.Button btnTest2;
+		private System.Windows.Forms.ListView listView1;
+	}
+}
---
+++ DGU_EnumToClass_Test/Form1.cs
@@ -0,0 +1,83 @@
+﻿using DGU.EnumToClass;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DGU_EnumToClass_Test
+{
+	public partial class Form1 : Form
+	{
+		/// <summary>
+		/// 테스트용 열거형1
+		/// </summary>
+		enum Test1
+		{
+			Test1_a = 0,
+			Test1_b,
+			Test1_c,
+		}
+
+		/// <summary>
+		/// 테스트용 열거형2
+		/// </summary>
+		enum Test2
+		{
+			Test2_a = 5,
+			Test2_b,
+			Test2_c,
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnTest1_Click(object sender, EventArgs e)
+		{
+			SetListView(new Test1());
+		}
+
+		private void btnTest2_Click(object sender, EventArgs e)
+		{
+			SetListView(new Test2());
+		}
+
+		private void SetListView(Enum typeData)
+		{
+			//열거형 지정
+			EnumList newEnum = new EnumList(typeData);
+
+			//컨트롤을 지우고
+			listView1.Clear();
+
+
+			//리스트뷰 바인드 시작
+			listView1.BeginUpdate();
+			//뷰모드
+			listView1.View = View.Details;
+
+			//열거형으로 컬럼 생성
+			for (int i = 0; i < newEnum.Count; ++i)
+			{
+				listView1.Columns.Add(newEnum.EnumMember[i].Name);
+			}
+
+			//아이템 추가
+			string[] sData = new string[newEnum.Count];
+			for (int i = 0; i < newEnum.Count; ++i)
+			{
+				sData[i] = newEnum.EnumMember[i].Index.ToString();
+			}
+			listView1.Items.Add(new ListViewItem(sData));
+
+			//리스트뷰 바인드 완료
+			listView1.EndUpdate();
+		}
+	}
+}
